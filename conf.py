a51868e7cf1764f11c388bced42b1cc730923203
omoikane@d4068f67-1e22-0410-a4c7-bcdb541354e5 <omoikane@d4068f67-1e22-0410-a4c7-bcdb541354e5>
2010-11-16 05:07:01 +0000

    themeをnatureにしてみる。

--- conf.py
+++ conf.py
@@ -91,6 +91,8 @@ exclude_dirnames = ['diff', 'orig', 'tools']
 # Options for HTML output
 # -----------------------
 
+html_theme = 'nature'
+
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 html_last_updated_fmt = '%b %d, %Y'
