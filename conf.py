db87be83c4266cb14b8c67a90af67befc9d27daa
Akihiro Uchida <uchida@ike-dyn.ritsumei.ac.jp>
2011-03-26 09:20:33 +0900

    LaTeX 出力の章の表示を改善 wiki LaTeXBuild クラスファイル周りの設定参照
    - 章のマークアップ修正
    - フッタでの表示の修正
    - 目次での付録の表示の修正

--- conf.py
+++ conf.py
@@ -169,6 +169,30 @@ latex_preamble = r'''
   \strong{Python Software Foundation}\\
   Email: \email{docs@python.org}
 }
+\makeatletter
+\renewcommand{\DOCH}{
+  \raggedleft\CNV\FmN{\@chapapp}\space
+  \CNoV\thechapter\CNV\FmN{\@chappos}
+  \par\nobreak\vskip40\p@
+}
+\fancypagestyle{normal}{
+  \fancyhf{}
+  \fancyfoot[LE,RO]{{\py@HeaderFamily\thepage}}
+  \fancyfoot[LO]{{\py@HeaderFamily\nouppercase{\rightmark}}}
+  \fancyfoot[RE]{{\py@HeaderFamily\nouppercase{\leftmark}}}
+  \fancyhead[LE,RO]{{\py@HeaderFamily \@title, \py@release}}
+  \renewcommand{\headrulewidth}{0.4pt}
+  \renewcommand{\footrulewidth}{0.4pt}
+  \def\chaptermark##1{\markboth{\@chapapp\space\thechapter\space\@chappos\space ##1}{}}
+}
+\renewcommand{\appendix}{\par
+  \setcounter{chapter}{0}%
+  \setcounter{section}{0}%
+  \gdef\@chapapp{\appendixname}%
+  \gdef\@chappos{}%
+  \gdef\thechapter{\@Alph\c@chapter}
+}
+\makeatother
 '''
 
 # Documents to append as an appendix to all manuals.
