69d7788d6d2fb52ab3c9a66caba9b6110d0c35e3
Akihiro Uchida <uchida@ike-dyn.ritsumei.ac.jp>
2011-12-09 19:13:48 +0900

    make.bat の修正
    - sphinx, docutils のレポジトリの変更
    - %PYTHON% を使わないように

--- make.bat
+++ make.bat
@@ -2,9 +2,9 @@
 setlocal
 
 set SVNROOT=http://svn.python.org/projects
-if "%PYTHON%" EQU "" set PYTHON=..\pcbuild\python
+::if "%PYTHON%" EQU "" set PYTHON=..\pcbuild\python
 if "%HTMLHELP%" EQU "" set HTMLHELP=%ProgramFiles%\HTML Help Workshop\hhc.exe
-if "%DISTVERSION%" EQU "" for /f "usebackq" %%v in (`%PYTHON% tools/sphinxext/patchlevel.py`) do set DISTVERSION=%%v
+if "%DISTVERSION%" EQU "" for /f "usebackq" %%v in (`tools/sphinxext/patchlevel.py`) do set DISTVERSION=%%v
 
 if "%1" EQU "" goto help
 if "%1" EQU "html" goto build
@@ -35,8 +35,9 @@ goto end
 
 :checkout
 ::svn co %SVNROOT%/external/Sphinx-0.6.5/sphinx tools/sphinx
-hg clone https://sphinx-ja.python-doc-ja.googlecode.com/hg/ tools/sphinx
-svn co %SVNROOT%/external/docutils-0.6/docutils tools/docutils
+hg clone https://bitbucket.org/birkenfeld/sphinx -r1.1 tools/sphinx
+::svn co %SVNROOT%/external/docutils-0.6/docutils tools/docutils
+svn export svn://svn.berlios.de/docutils/tags/docutils-0.7/docutils tools/docutils
 svn co %SVNROOT%/external/Jinja-2.3.1/jinja2 tools/jinja2
 svn co %SVNROOT%/external/Pygments-1.3.1/pygments tools/pygments
 goto end
@@ -53,7 +54,7 @@ goto end
 if not exist build mkdir build
 if not exist build\%1 mkdir build\%1
 if not exist build\doctrees mkdir build\doctrees
-cmd /C %PYTHON% tools\sphinx-build.py -b%1 -dbuild\doctrees . build\%*
+tools\sphinx-build.py -b%1 -dbuild\doctrees . build\%*
 if "%1" EQU "htmlhelp" "%HTMLHELP%" build\htmlhelp\python%DISTVERSION:.=%.hhp
 goto end
 
