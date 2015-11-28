7161b164def1f71347ffda79c9982b0c288734b0
INADA Naoki <songofacandy@gmail.com>
2015-11-29 02:42:25 +0900

    tool: move to Python 3

--- tools/extensions/jpsupport.py
+++ tools/extensions/jpsupport.py
@@ -1,14 +1,14 @@
 from docutils.nodes import Text, paragraph
 import re
 
-_PAT = re.compile(ur"([^\u0000-\u00ff])\n([^\u0000-\u00ff])")
+_PAT = re.compile(r"([^\u0000-\u00ff])\n([^\u0000-\u00ff])")
 
 def trunc_whitespace(app, doctree, docname):
     if not app.config.japanesesupport_trunc_whitespace:
         return
     for node in doctree.traverse(Text):
         if isinstance(node.parent, paragraph):
-            newtext = _PAT.sub(ur"\1\2", node.astext())
+            newtext = _PAT.sub(r"\1\2", node.astext())
             node.parent.replace(node, Text(newtext))
 
 def setup(app):
